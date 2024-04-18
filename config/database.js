c592e42efa9295c11c708b0388e48bed10aea67f
Ayush Dama <114308646+ayushdama11@users.noreply.github.com>
2024-04-18 19:28:31 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
-/**
-* this configuration for connect our application with mongodb
-*/
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/restaurant', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-    if (err) {
-        console.error('Error connecting to MongoDB:', err);
-    } else {
-        console.log('Connected to MongoDB');
-        // You can start using your mongoose models or perform other operations here.
-    }
-});
+const connectWithRetry = () => {
+    mongoose.connect('mongodb://localhost:27017/restaurant', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+        if (err) {
+            console.error('Error connecting to MongoDB:', err);
+            // Retry connection after a delay
+            setTimeout(connectWithRetry, 5000);
+        } else {
+            console.log('Connected to MongoDB');
+            // You can start using your mongoose models or perform other operations here.
+        }
+    });
+};
+
+connectWithRetry();
